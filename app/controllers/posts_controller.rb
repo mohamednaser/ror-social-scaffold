a23342fd01029e494bd432dde07d2327636e469e
Mohamed Naser <nc_m.naser@hotmail.com>
2020-02-27 20:42:29 +0200

    return friends and curent user posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -20,7 +20,10 @@ class PostsController < ApplicationController
   private
 
   def timeline_posts
-    @timeline_posts ||= Post.all.ordered_by_most_recent.includes(:user)
+    prefered_users = current_user.friends.pluck(:id) 
+    prefered_users << current_user.id
+    
+    @timeline_posts ||= Post.where(user_id: prefered_users).ordered_by_most_recent.includes(:user)
   end
 
   def post_params
